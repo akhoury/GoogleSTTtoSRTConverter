7f7d58769f65db39ee2f3c59a36931f4b08d1c7f
Aziz Khoury <bentael@gmail.com>
2019-11-13 13:40:09 +0200

    chunk out lines with over 14 words per line

--- src/convertGSTTToSRT.ts
+++ src/convertGSTTToSRT.ts
@@ -1,4 +1,5 @@
 export function convertGSTTToSRT(string: string) {
+
     class hourRepresentation {
         private hours: number;
         private minutes: number;
@@ -21,9 +22,9 @@ export function convertGSTTToSRT(string: string) {
 
         toString() {
             return String(this.hours).padStart(2, '0') + ':'
-            + String(this.minutes).padStart(2, '0') + ':'
-            + String(this.seconds).padStart(2, '0') + ','
-            + this.nanos.substr(0,3);
+                + String(this.minutes).padStart(2, '0') + ':'
+                + String(this.seconds).padStart(2, '0') + ','
+                + this.nanos.substr(0,3);
         }
 
     }
@@ -37,12 +38,34 @@ export function convertGSTTToSRT(string: string) {
         return typeof toBeDetermined === 'object';
     }
 
+    function chunkArray (array: any, size: number) {
+        var i, j, arrays = [];
+        for (i = 0, j = array.length; i < j; i += size) {
+            arrays.push(array.slice(i, i + size));
+        }
+        return arrays;
+    }
 
+    function chunkLine (line: any) {
+        var chunks = chunkArray(line.alternatives[0].words, MAX_WORDS_COUNT_IN_1_LINE);
+        var lines = [];
+
+        for (i = 0; i < chunks.length; i += 1) {
+            var words = chunks[i];
+            lines.push({
+                alternatives: [{ words: words, transcript: words.map(function (word: any) { return word.word; }).join(' ') }]
+            });
+        }
+        return lines;
+    }
+
+    const MAX_WORDS_COUNT_IN_1_LINE = 14;
     var obj = JSON.parse(string);
     var i = 1;
     var result = '';
     const array = obj.response ? obj.response.results : obj.results; // The object can be the full response or the response object
-    for (const line of array) {
+
+    function processLine (line: any) {
         result += i++;
         result += '\n';
         var word = line.alternatives[0].words[0];
@@ -53,6 +76,15 @@ export function convertGSTTToSRT(string: string) {
         result += time.toString() + '\n';
         result += line.alternatives[0].transcript + '\n\n';
     }
+
+    for (const line of array) {
+        if (line.alternatives[0].words.length <= MAX_WORDS_COUNT_IN_1_LINE) {
+            processLine(line)
+        } else {
+            chunkLine(line).forEach(processLine);
+        }
+    }
+
     return result;
 }
 
